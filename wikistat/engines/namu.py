a04003c205194681e5f3dfa1546692da3f9cd03a
Taeho Kim <dittos@gmail.com>
2015-04-19 09:20:35 +0900

    Add NamuWiki page count

--- wikistat/engines/namu.py
+++ wikistat/engines/namu.py
@@ -6,10 +6,24 @@ import requests
 
 KST = pytz.timezone('Asia/Seoul')
 
+def http_get(url):
+    return requests.get(url, headers={'User-Agent': 'Mozilla/5.0 ()'}, timeout=10, verify=False)
+
+PAGE_COUNT_MACRO = '[[PageCount]]'
+
 def collect(options):
     data = {}
-    resp = requests.get('https://namu.wiki/RecentChanges', headers={'User-Agent': 'Mozilla/5.0 ()'}, timeout=10, verify=False)
-    data['page_count'] = None
+    resp = http_get('https://namu.wiki/raw/FrontPage')
+    prefix = None
+    for line in resp.text.splitlines():
+        if PAGE_COUNT_MACRO in line:
+            prefix = line.split(PAGE_COUNT_MACRO, 1)[0]
+            break
+    assert prefix is not None
+    resp = http_get('https://namu.wiki/w/FrontPage')
+    match = re.search(re.escape(prefix) + '(\d+)', resp.text)
+    data['page_count'] = int(match.group(1))
+    resp = http_get('https://namu.wiki/RecentChanges')
     changes = []
     # TODO: 너무 대충 파싱하나?
     for match in re.findall(r"<td>(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})</td>", resp.text):
