6af5422e2e2371fb3e2287ce1424962704eb1ef3
Taeho Kim <dittos@gmail.com>
2015-04-24 00:19:59 +0900

    Add logging

--- wikistat/collect.py
+++ wikistat/collect.py
@@ -1,5 +1,7 @@
 import datetime
 import json
+import os
+import time
 import jinja2
 import pytz
 import yaml
@@ -13,10 +15,11 @@ ENGINES = {
     'namu': namu,
 }
 
-def main(site_config_path, output_html_path):
+def main(site_config_path, output_html_path, log_dir):
     with open(site_config_path) as fp:
         sites = yaml.load_all(fp.read())
     results = []
+    log_entry = {'stats': {}}
     for site in sites:
         site = dict(site) # copy
         engine = ENGINES[site['engine']]
@@ -35,15 +38,28 @@ def main(site_config_path, output_html_path):
         site['page_count'] = data['page_count']
         site['freq'] = nchanges / period_days
         results.append(site)
+        log_entry['stats'][site['id']] = {
+            'page_count': site['page_count'],
+            'freq': site['freq'],
+        }
 
+    t = time.time()
+    log_entry['t'] = t
     loader = jinja2.FileSystemLoader('.')
     env = jinja2.Environment(loader=loader)
     html = env.get_template('template.html').render(
         results=results,
-        timestamp=datetime.datetime.now(),
+        timestamp=datetime.datetime.fromtimestamp(t),
     )
     with open(output_html_path, 'w') as fp:
         fp.write(html.encode('utf-8'))
 
+    try:
+        os.mkdir(log_dir, 0700)
+    except OSError:
+        pass # already exist
+    with open(os.path.join(log_dir, 'wikistat.log.' + datetime.date.today().strftime('%Y%m%d')), 'a') as fp:
+        fp.write(json.dumps(log_entry) + '\n')
+
 if __name__ == '__main__':
-    main('sites.yml', 'index.html')
+    main('sites.yml', 'index.html', 'logs')
